a49da73db4ae0e0af2da84bd3e469abf2f224f46
igorj <igor.castro_ext@light.com.br>
2025-10-15 17:10:23 -0300

    ajuste na unidade

--- src/main/java/org/sus/infraestructure/entity/UnidadeEntity.java
+++ src/main/java/org/sus/infraestructure/entity/UnidadeEntity.java
@@ -2,16 +2,13 @@ package org.sus.infraestructure.entity;
 
 
 import jakarta.persistence.*;
-import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Duration;
 import java.time.LocalTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Entity
 @Table(name = "unidade")
 public class UnidadeEntity implements Serializable {
@@ -31,4 +28,39 @@ public class UnidadeEntity implements Serializable {
     @Column(name = "horario_fechamento")
     private LocalTime horaFecha;
     private Duration duracao;
+
+    public UnidadeEntity() {
+    }
+
+    public UnidadeEntity(Long id, String nome, String endereco, Boolean ativo,
+                         LocalTime horaAbre, LocalTime horaFecha, Duration duracao) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.ativo = ativo;
+        this.horaAbre = horaAbre;
+        this.horaFecha = horaFecha;
+        this.duracao = duracao;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+
+    public Boolean getAtivo() { return ativo; }
+    public void setAtivo(Boolean ativo) { this.ativo = ativo; }
+
+    public LocalTime getHoraAbre() { return horaAbre; }
+    public void setHoraAbre(LocalTime horaAbre) { this.horaAbre = horaAbre; }
+
+    public LocalTime getHoraFecha() { return horaFecha; }
+    public void setHoraFecha(LocalTime horaFecha) { this.horaFecha = horaFecha; }
+
+    public Duration getDuracao() { return duracao; }
+    public void setDuracao(Duration duracao) { this.duracao = duracao; }
 }
